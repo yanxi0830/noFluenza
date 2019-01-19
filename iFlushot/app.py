f5dc71b6acb51ce09b6f03b87bbca7feca677453
Xi Yan <yanxi970830@gmail.com>
2019-01-19 17:28:22 -0500

    db

--- iFlushot/app.py
+++ iFlushot/app.py
@@ -3,6 +3,7 @@
 from scripts import tabledef
 from scripts import forms
 from scripts import helpers
+from scripts import db as db
 from flask import Flask, redirect, url_for, render_template, request, session
 import json
 import sys
@@ -16,62 +17,13 @@ app = Flask(__name__)
 # -------- Login ------------------------------------------------------------- #
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    if not session.get('logged_in'):
-        form = forms.LoginForm(request.form)
-        if request.method == 'POST':
-            username = request.form['username'].lower()
-            password = request.form['password']
-            if form.validate():
-                if helpers.credentials_valid(username, password):
-                    session['logged_in'] = True
-                    session['username'] = username
-                    return json.dumps({'status': 'Login successful'})
-                return json.dumps({'status': 'Invalid user/pass'})
-            return json.dumps({'status': 'Both fields required'})
-        return render_template('login.html', form=form)
-    user = helpers.get_user()
-    return render_template('home.html', user=user)
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return redirect(url_for('login'))
-
-
-# -------- Signup ---------------------------------------------------------- #
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if not session.get('logged_in'):
-        form = forms.LoginForm(request.form)
-        if request.method == 'POST':
-            username = request.form['username'].lower()
-            password = helpers.hash_password(request.form['password'])
-            email = request.form['email']
-            if form.validate():
-                if not helpers.username_taken(username):
-                    helpers.add_user(username, password, email)
-                    session['logged_in'] = True
-                    session['username'] = username
-                    return json.dumps({'status': 'Signup successful'})
-                return json.dumps({'status': 'Username taken'})
-            return json.dumps({'status': 'User/Pass required'})
-        return render_template('login.html', form=form)
-    return redirect(url_for('login'))
-
+    return render_template('login.html')
 
 # -------- Settings ---------------------------------------------------------- #
-@app.route('/settings', methods=['GET', 'POST'])
-def settings():
-    if session.get('logged_in'):
-        if request.method == 'POST':
-            password = request.form['password']
-            if password != "":
-                password = helpers.hash_password(password)
-            email = request.form['email']
-            helpers.change_user(password=password, email=email)
-            return json.dumps({'status': 'Saved'})
-        user = helpers.get_user()
-        return render_template('settings.html', user=user)
+@app.route('/patient', methods=['POST'])
+def add_patient():
+    req_data = request.get_json()
+    db.add_patient(req_data)
     return redirect(url_for('login'))
 
 
--- iFlushot/scripts/db.py
+++ iFlushot/scripts/db.py
@@ -1,8 +1,13 @@
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://iflushot:qwe123@ds054999.mlab.com:54999/iflushot")
+myclient = pymongo.MongoClient("mongodb://iflu:qwe123@ds030719.mlab.com:30719/iflushot")
 
 mydb = myclient["iflushot"]
 
 print(mydb.list_collection_names())
 
+
+patient_collection = mydb.get_collection("patients")
+
+def add_patient(req_data):
+    patient_collection.insert_one(req_data)
